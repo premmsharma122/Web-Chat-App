1b784a417293de9bdf2ad242117d7ffd7d907ee0
Prem Sharma <sonusharma4237@gmail.com>
2025-08-19 03:28:40 +0530

    Node Env

--- server/server.js
+++ server/server.js
@@ -156,5 +156,10 @@ app.use("/api/users", userRouter);
 app.use("/api/messages", messageRouter);
 
 // CONNECT DB AND START SERVER
-connectDB();
+await connectDB();
+if(process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;  
+    server.listen(PORT, () => console.log("Server running on port:", PORT));
+}
+
 export default server;
